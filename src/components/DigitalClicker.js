67a84e48b57eb152b55215012c7f256d7f01916d
Trent Moss <trentmoss05@gmail.com>
2019-05-24 03:13:13 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+<<<<<<< HEAD
 class DigitalClicker extends React.Component {
+=======
+class YouTubeDebugger extends React.Component {
+>>>>>>> 3a2813d52330bc1933d5a99db86a0923fe12a178
   constructor() {
     super();
 
     this.state = {
+<<<<<<< HEAD
       timesClicked: 0,
     };
   }
@@ -18,8 +23,69 @@ class DigitalClicker extends React.Component {
   render() {
     return (
       <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+=======
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    };
+  }
+
+  handleChangeBitrate = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    });
+  };
+
+  handleChangeResolution = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.settings.video,
+          resolution: '720p'
+        }
+      }
+    });
+
+    /*
+    Or this can be defined using the Object.assign method:
+    
+    this.setState({
+      settings: Object.assign({}, this.state.settings, {
+        video: Object.assign({}, this.state.settings.video, {
+          resolution: '720p'
+        })
+      })
+    });
+    
+    */
+  };
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.handleChangeBitrate}>
+          Change bitrate
+        </button>
+        <button className="resolution" onClick={this.handleChangeResolution}>
+          Change resolution
+        </button>
+      </div>
+>>>>>>> 3a2813d52330bc1933d5a99db86a0923fe12a178
     );
   }
 }
 
+<<<<<<< HEAD
 export default DigitalClicker;
+=======
+export default YouTubeDebugger;
+>>>>>>> 3a2813d52330bc1933d5a99db86a0923fe12a178
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 
 class YouTubeDebugger extends React.Component {
@@ -65,3 +66,55 @@ class YouTubeDebugger extends React.Component {
 }
 
 export default YouTubeDebugger;
+=======
+import React from 'react';
+
+class YoutubeDebugger extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      errros: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    };
+  }
+  
+  handleChangeBitrate = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    });
+  };
+  
+  handleChangeResolution = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.settings.video,
+          resolution: '720p'
+        }
+      }
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.handleChangeBitrate}>Change bitrate</button>
+        <button className="resolution" onClick={this.handleChangeResolution}>Change resolution</button>
+      </div>
+    );
+  }
+}
+
+export default YoutubeDebugger;
+>>>>>>> 3a2813d52330bc1933d5a99db86a0923fe12a178
